e783a71b76fd1fcfcb8560c3089910dad485a4a4
Wasswa William <wwasswa@must.ac.ug>
2016-10-25 15:23:32 +0300

    ok

---
+++ index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>SafePal</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		
+		
+		
+		<link href="admin/css/style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="admin/febe/style.css" type="text/css" media="screen" charset="utf-8">
+<script src="admin/argiepolicarpio.js" type="text/javascript" charset="utf-8"></script>
+<script src="admin/js/application.js" type="text/javascript" charset="utf-8"></script>	
+<!--sa poip up-->
+<link href="admin/src/report/facebox.css" media="screen" rel="stylesheet" type="text/css" />
+   <script src="admin/lib/jquery.js" type="text/javascript"></script>
+  <script src="admin/src/report/facebox.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    jQuery(document).ready(function($) {
+      $('a[rel*=facebox]').facebox({
+        loadingImage : 'src/loading.gif',
+        closeImage   : 'src/closelabel.png'
+      })
+    })
+  </script>
+  
+      <style type="text/css">
+
+        ul{
+            padding: 0;
+            list-style: none;
+            background: #ffffff;
+			text-align: right;
+			margin-top:10px
+        }
+        ul li{
+            display: inline-block;
+            position: relative;
+            line-height: 21px;
+            text-align: left;
+        }
+        ul li a{
+            display: block;
+            padding: 8px 25px;
+            color: #333;
+            text-decoration: none;
+
+        }
+        ul li a:hover{
+            color: #fff;
+            background: #939393;
+        }
+
+        ul li ul.dropdown{
+            min-width: 100%; /* Set width of the dropdown */
+            background: #f2f2f2;
+            display: none;
+            position: absolute;
+            z-index: 999;
+            left: 0;
+        }
+
+        ul li:hover ul.dropdown{
+
+            display: block; /* Display the dropdown */
+        }
+        ul li ul.dropdown li{
+
+            display: block;
+
+        }
+
+    </style>
+<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:700italic,400,300,700' rel='stylesheet' type='text/css'>
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+<script type="text/javascript">
+<!--
+    function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+//-->
+</script>
+		<style type="text/css">
+		body {
+	background-color: #CCC;
+}
+        </style>
+<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+
+	</head>
+	<body bgcolor="#808080" class="homepage">
+
+	<!-- Header -->
+<div id="header">
+			<div class="container">
+					
+				<!-- Logo -->
+										
+<div style="position:relative; left: 0; top: 0;">    
+<img src=".\images\logo1.png" style="position:relative;width:140px;height:90px;float:left; margin-top:-25px;">
+<div style="position:relative; left: 30; top: 0;" id="logo">
+						<h1 align="left"><a href="#">SafePal</a></h1> 
+ 
+					</div>
+</div>
+				
+						
+				
+				 <ul>
+            <div style="margin-top: -70px;">
+           
+            <li class="active">
+
+                <a href="#">Report an Incidence &#9662;</a>
+
+                <ul class="dropdown">
+
+                    <li><a  rel="facebox" href="reportingincidence.php">For myself</a></li>
+
+                    <li><a rel="facebox" href="reportingincidencefrd.php">For a Friend</a></li>
+
+                    
+                </ul>
+
+            </li>
+
+            <li><a href="connectwithus.php">Connect with Us</a></li>
+ <li><a href="help.php">Help</a></li>
+        </ul>
+				
+				
+				
+				
+
+			</div>
+		</div>
+	<!-- Header -->
+		
+	<!-- Banner -->
+		 <div id="banner">
+                  <div style="position:relative; left: 0; top: 0;">
+ <a href=".\images\Safepal.apk" download = "Safepal.apk">
+<img src=".\images\ee.png"
+style="position:relative;width:180px;height:80px;float:right;"> </a>
+</div>
+                <div class="container">
+
+              </div>
+                </div>
+	<!-- /Banner -->
+
+	<!-- Main -->
+<div id="page">
+
+			<!-- Extra -->
+			<div id="marketing" class="container">
+				<div class="row">
+					<div class="3u">
+						<section>
+							<header>
+								<h2>sexual violence</h2>
+							</header>
+							<p class="subtitle">Know that the sexual violence wasn't your fault.</p>
+<div id="content1">                            
+<p style="text-align:justify">                            
+<ul style="list-style-type:square">
+<li>Whatever happened, wasn't your fault. No one has the right to sexually violet you.  The blame for a rape lies solely with the rapist. The truth is that what a person wears, what a person says, or how a person acts is never a justification for sexual violence. </li>
+<a href="#" onclick="toggle_visibility('foo');">More/Less</a>
+<div id="foo" style="display:none;">
+<li>Most people are raped by people they know. You do not have to protect the perpetrator. Make protecting yourself your priority. Don't worry about protecting the person who raped you. </li>		
+<li>If you want to report the crime to the police, do so. Reporting sexual violence may help protect others from that person — and may help you feel a little less like you were a victim. </li>
+<li>But making a report to the police may be difficult. If you need help reporting to police contact [Drop down box of contacts for CSOs] and if you don't feel comfortable reporting it, you don't have to. You may prefer to get advice about what to do from an experienced adult who can be sympathetic to you. Do whatever helps you to feel safe and heal without blaming yourself. </li>
+</ul>
+</p>
+</div>
+
+                        </section>
+				  </div>
+					<div class="3u">
+						<section>
+							<header>
+								<h2>seek medical care</h2>
+							</header>
+							<p class="subtitle">It is critical that you seek medical care.</p>
+<p style="text-align:justify">
+<ul style="list-style-type:square">
+<li>40% of rape survivors develop sexually transmitted diseases as a result of sexual assault.</li>
+<li>You should get medical attention right away without changing your clothes, showering, douching, or washing. It can be hard not to clean up, of course — it's a natural human instinct to wash away all traces of a sexual assault. But being examined right away is the best way to ensure you get proper medical treatment.</li>
+<a href="#" onclick="toggle_visibility('foo');">More/Less</a>
+<div id="foo" style="display:none;">
+<li>Immediate medical attention helps when people decide to report the crime, providing evidence needed to prosecute the rapist if a criminal case is pursued. If you've been raped and think you don't want to report it, you could change your mind later and having the results of a medical exam can help you do this.</li>
+<li>Even if you don't get examined right away, it doesn't mean you can't get a checkup later. It's always best to see a doctor immediately after a rape, of course. But a person can still go to a doctor or local clinic to get checked out for STDs, pregnancy, or injuries any time after being raped. In some cases, doctors can even gather evidence several days after a rape has occurred.</li>
+<li>A medical professional will test you for STDs, including HIV/AIDS. These tests may involve taking blood or saliva samples. Although the thought of getting an STD after a rape is extremely scary, the quicker a person finds out about any infection, the more effectively he or she can be treated. Doctors can start you on immediate treatment courses for STDs, including HIV/AIDS that will greatly increase your protection against developing these diseases.
+</li>
+<p style="text-align:justify">All doctors and nurses will deal with your medical needs confidentially, and they will not inform anyone without your consent.</p>
+</ul>
+</p>
+						</section>
+					</div>
+					<div class="3u">
+						<section>
+							<header>
+							<h2>Speak with a caring person </h2>
+							</header>
+							<p class="subtitle">Speaking with a caring person will help address some of your feelings.</p>
+
+<p style="text-align:justify">
+<ul style="list-style-type:square">
+<li>Talk to a trained counsellor at [Drop down box of contacts for Counselors/ CSOs] why?</li>
+<li>4 out of 5 rape victims subsequently suffer from chronic physical or psychological conditions.</li>
+<a href="#" onclick="toggle_visibility('foo');">More/Less</a>
+<div id="foo" style="display:none;">
+<li>Rape survivors are 13 times more likely to attempt suicide than are people who have not been victims of a crime.</li>
+<li>Rape isn't just physically damaging, it can be emotionally traumatic as well. The right emotional attention, care, and support can you begin the healing process and prevent lingering problems later on.</li>
+<li>Someone who has been raped might feel a lot of things: angry, degraded, frightened, numb, or confused. It's also normal for someone who has been raped to feel ashamed or embarrassed. Some people withdraw from friends and family. Others don't want to be alone. Some feel depressed, anxious, or nervous.</li>
+<li>Sometimes the feelings surrounding rape may show up in physical ways, such as trouble sleeping or eating. It may be hard to concentrate in school or to participate in everyday activities. Sometimes it may feel like you'll never get over the trauma of the rape. Experts often refer to these emotions and their physical side effects as rape trauma syndrome. The best way to work through them is with professional help.</li>
+</ul>
+</p>
+
+						</section>
+					</div>
+
+					<div class="3u">
+						<section>
+							<header>
+								<h2>Signs of Sexual Violence</h2>
+							</header>
+							<p class="subtitle">Be aware that behavioral symptoms are far more common than physical ones.</p>
+<p><b>Physical:</b></p>
+<p style="text-align:justify">
+<ul style="list-style-type:square">
+<li>Torn, stained, or bloody underclothing</li>
+<li>Pain or itching in genital area</li>
+<a href="#" onclick="toggle_visibility('foo');">More/Less</a>
+<div id="foo" style="display:none;">
+<li>Difficulty walking or sitting</li>
+<li>Bruises or bleeding in genital areas</li>
+<li>Frequent urinary or yeast infections</li>
+<li>Sexually transmitted diseases </li>
+</ul>
+<p><b>Behavioral:</b></p>
+<ul style="list-style-type:square">
+<li>Chronic depression</li>
+<li>Promiscuity</li>
+<li>Poor self-esteem/lack of confidence</li>
+<a href="#" onclick="toggle_visibility('foo');">More/Less</a>
+<div id="foo" style="display:none;">
+<li>Role reversal with parent</li>
+<li>Problems with peers</li>
+<li>Massive weight change</li>
+<li>Suicidal thoughts or actions</li>
+<li>Sudden problems in school</li>
+<li>Inappropriate or premature sexual behavior</li>
+<li>Fear of physical contact</li>
+</ul>
+</p>
+						</section>
+					</div>
+				</div>
+			</div>
+			<!-- /Extra -->
+				
+			<!-- Main -->
+						
+					</div>
+				</div>
+			</div>
+			<!-- Main -->
+
+	</div>
+	<!-- /Main -->
+
+	<!-- /Featured -->
+
+
+				</div>
+			</div>
+		</div>
+	<!-- /Footer -->
+
+	<!-- Copyright -->
+		<div id="copyright" class="container">
+Copyright	@ <a href="http://safepal.com">SafePal Designers : <br><a href="login.php">login into Safe Pal</a></a> 
+	</div>
+
+
+	</body>
+</html>
